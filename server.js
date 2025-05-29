fd29aea3bc3e4114f201291a26d383c78ac95a6b
Cristian Castro <cristianjunnior.castrovazquez@gmail.com>
2025-05-29 19:48:26 -0300

    se cambia fastify

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const fastify = require('fastify')({ logger: true });
+const path = require('path');
+const fs = require('fs');
+
+// Registrar el plugin multipart
+fastify.register(require('@fastify/multipart'));
 
 fastify.get('/', async (request, reply) => {
   return { message: 'Hello from Fastify in Docker!' };
+});
+
+// Nueva ruta para subir archivos
+fastify.post('/upload', async (request, reply) => {
+  const data = await request.file();
+  const uploadDir = path.join(__dirname, 'uploads');
+  
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+  
+  const filePath = path.join(uploadDir, data.filename);
+  await fs.promises.writeFile(filePath, await data.toBuffer());
+  
+  return { message: 'Archivo subido exitosamente', filename: data.filename };
 });
 
 const start = async () => {
